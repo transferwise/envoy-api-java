4cb72073280d2bc611b0c648ea77b59c79e9d466
Robin Palotai <robin.palotai@transferwise.com>
2022-11-07 15:01:49 +0100

    Manually update SHAs and some changes.
    Next: see if pulling in upstream updater script "just works".
    Also if using buf to generate java api is simpler?

--- tools/update-api.sh
+++ tools/update-api.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+set -x
+
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${__dir}/API_SHAS"
@@ -55,10 +57,10 @@ cp -r opencensus-proto-*/src/opencensus/proto/* "${protodir}/opencensus/proto"
 curl -sL https://github.com/prometheus/client_model/archive/${PROMETHEUS_SHA}.tar.gz | tar xz --wildcards -T include-proto
 cp client_model-*/metrics.proto "${protodir}"
 
-curl -sL https://github.com/cncf/udpa/archive/${UDPA_SHA}.tar.gz | tar xz --wildcards -T include-proto
+curl -sL https://github.com/cncf/xds/archive/${UDPA_SHA}.tar.gz | tar xz --wildcards -T include-proto
 mkdir -p "${protodir}/udpa"
 mkdir -p "${protodir}/xds"
-cp -r udpa-*/udpa/* "${protodir}/udpa"
-cp -r udpa-*/xds/* "${protodir}/xds"
+cp -r xds-*/udpa/* "${protodir}/udpa"
+cp -r xds-*/xds/* "${protodir}/xds"
 
 popd >/dev/null
